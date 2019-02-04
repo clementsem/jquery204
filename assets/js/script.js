55f157e12070e62cce42e7cc2edbee23587935af
clementsem <clementctes@gmail.com>
2019-02-04 09:48:29 +0100

    exo4

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('#green').click(function(){
+      $('#text').css('color', 'green');
+  });
+  $('#red').click(function(){
+      $('#text').css('color', 'red');
+  });
+  $('#blue').click(function(){
+      $('#text').css('color', 'blue');
+  });
+})
